a121eba3990242e21606c1a2ed3df05f0c43d4a6
Carlos Martínez <cmtinz@gmail.com>
2018-11-26 21:09:55 -0300

    React frontend

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg')
 
-const pool = new Pool()
+const pool = new Pool({
+  user: "tcitdb",
+  host: "127.0.0.1",
+  database: "tcitdb",
+  password: "evaristo78",
+  port: "5432"
+})
 
 module.exports = {
   query: (text, params, callback) => {
--- routes/posts.js
+++ routes/posts.js
@@ -1,24 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db')
+var bodyParser = require('body-parser')
+
+router.use(bodyParser.json())
 
 /* Filtrar posts por nombre localmente */
-router.get('/:name', function(req, res, next) {
-  res.send('named resource');
+router.post('/search', async (req, res, next) => {
+  const queryName = req.body.queryName
+  const rows = await db.query(`select * from posts where post_name ilike '%${req.body.queryName}%'`)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json(rows.rows)
 });
 
 /* Listar posts */
-router.get('/', function(req, res, next) {
-  res.send('all posts');
-});
+
+router.get('/', async (req, res) => {
+  const rows = await db.query('SELECT * FROM posts')
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json(rows.rows)
+})
+
 
 /* Insertar posts */
-router.post('/', function(req, res, next) {
-  res.send('insert posts');
-});
+// insert into posts (post_id, post_name, post_description) values (default, 'Post 2', 'Descripción');
+router.post('/', async (req, res, next) => {
+  const postName = req.body.postName
+  const postDescription = req.body.postDescription
+  const rows = await db.query(`insert into posts (post_id, post_name, post_description) values (default, '${postName}', '${postDescription}')`)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json(rows)
+  console.log(rows)
+})
 
 /* Eliminar posts */
-router.delete('/:id', function(req, res, next) {
-  res.send('deleting posts');
+router.delete('/:id', async (req, res, next) => {
+  const rows = await db.query(`delete from posts where post_id = ${req.params.id}`)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json(rows)
+  console.log(rows)
 });
 
 module.exports = router;
